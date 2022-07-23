917a769da6fc3903045cfeebcc6b84acb5e703b8
hejingyuan <jingyuan.he@mail.utoronto.ca>
2022-07-22 21:14:18 -0400

    made plot function for reading maps

--- HistorySimulation/simulation.py
+++ HistorySimulation/simulation.py
@@ -7,6 +7,7 @@ from urllib.request import urlopen
 import pyarrow as pa
 import pyarrow.parquet as pq
 from dateutil.relativedelta import *
+from matplotlib import pyplot as plt
 
 from PolygonFunctionsCrypto import Get_historical_data
 
@@ -23,7 +24,7 @@ def construct_illiquidity(data, length):
     #eth_dump_liquidity_sort = np.argsort(liquidity_index)
     eth_dump_sort = eth_dump_np[eth_dump_np[:, 8].argsort()]
     eth_dump_sort_length = eth_dump_sort[0:length, :]
-    return eth_dump_sort_length
+    return eth_dump_sort_length, eth_dump_np
 
 
 #Function convert the numpy to parquet file and store it
@@ -65,33 +66,38 @@ def margin_calculator(name, length, buying_name, selling_name):
     a = a.to_numpy()
     return a.sum()/length
 
-collect_times = 600
-selling_time = 1
+collect_times = 100
+selling_time = 30
 a = []
-starting_month = datetime.date(2021, 6, 20)
+starting_month = datetime.date(2021, 1, 20)
 date_list = [starting_month + relativedelta(months=+x) for x in range(11)]
+
+"""
+The longer the selling time clearly indicates the bigger risk that holding such asset would carry, no clear method to offset it"""
 #[0.0008910833452690574, 0.0007820495657642039, 0.0006370691982119203, 0.000922312342715534, 0.0005484461393270147, 0.0006075630654588654, 0.0002326166320267455, 8.34679793868154e-05, 0.0001399186734747146, 0.0001310090039397384, 6.367059632772908e-05]
 #[0.0008610277487244006, 0.0021107605852253406, 0.0007997255530908132, 0.0004217144022950859, 0.0003675114988143117, 0.00035448042911426307, 0.00018105105263857435, 8.35535151883573e-06, -3.1633374597974755e-06, 4.259964468678959e-05, 4.801876391391465e-05]
 #[0.00035448042911426307, 0.00018105105263857435, 8.35535151883573e-06, -3.1633374597974755e-06, 4.259964468678959e-05, 4.801876391391465e-05, -1.0940526399025674e-05, -8.298291432314898e-05, 5.504807386407333e-06, -1.9252704672376194e-05, 0.00036230234856302]
 
 
 #iterate through 11 months of data
-for i in date_list:
-    eth_dump = Get_historical_data("X:ETHUSD", i, i+relativedelta(months=+1), span="minute")
-    eth_dump_np = eth_dump.to_numpy()
+def run_year():
+    for i in date_list:
+        eth_dump = Get_historical_data("X:ETHUSD", i, i+relativedelta(months=+1), span="minute")
+        eth_dump_np = eth_dump.to_numpy()
 
-    eth_illiq = construct_illiquidity(eth_dump_np, collect_times)
-    numpy_to_parquet(eth_illiq, "eth_dump")
-    selling_price = find_selling(eth_dump_np, eth_illiq, selling_time)
-    # selling_price2 = find_selling(eth_dump_np, eth_illiq, selling_time+1)
-    add_column_to_parquet(selling_price, "eth_dump", "price_after")
-    # add_column_to_parquet(selling_price2, "eth_dump", "price_after_2")
-    a.append(margin_calculator("eth_dump", collect_times, "close", "price_after"))
+        eth_illiq = construct_illiquidity(eth_dump_np, collect_times)
+        numpy_to_parquet(eth_illiq, "eth_dump")
+        selling_price = find_selling(eth_dump_np, eth_illiq, selling_time)
+        # selling_price2 = find_selling(eth_dump_np, eth_illiq, selling_time+1)
+        add_column_to_parquet(selling_price, "eth_dump", "price_after")
+        # add_column_to_parquet(selling_price2, "eth_dump", "price_after_2")
+        a.append(margin_calculator("eth_dump", collect_times, "close", "price_after"))
 
-print(a)
+    print(a)
 
 
-parquet_to_csv("eth_dump")
+    parquet_to_csv("eth_dump")
+
 
 
  
